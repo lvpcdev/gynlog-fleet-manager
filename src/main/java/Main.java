75a8112fc1475e8df04ff88692c6fd37e3433738
Usuario <aluno@gmail.com>
2025-11-26 20:57:57 -0300

    Finalizando o rascunho swing de cadastro de veículos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,5 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            CadastroVeiculosView tela = new CadastroVeiculosView();
-            tela.setVisible(true);
-        });
-
-    }
 
 }
