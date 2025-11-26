39b48f67e89aca0946778ff513c77cea91458b85
Usuario <aluno@gmail.com>
2025-11-25 21:57:19 -0300

    Inicio do Swing - conriação da janela de cadastro de veículos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
+
+import View.CadastroVeiculosView;
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    public static void main(String[] args) {
+        CadastroVeiculosView tela = new CadastroVeiculosView();
+        tela.setVisible(true);
+
+        JPanel painelCadastroVeiculo = new JPanel(new GridBagLayout());
+
+    }
+
 }
---
+++ src/main/java/View/CadastroVeiculosView.java
@@ -0,0 +1,19 @@
+package View;
+
+import javax.swing.*;
+
+
+
+public class CadastroVeiculosView extends JFrame {
+
+    public CadastroVeiculosView(){
+        setTitle("Cadastro de Veículos");
+        setSize(800,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+
+
+
+}
