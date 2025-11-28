2b4343cd551d3778980551ed0782727d36588927
Usuario <abc@gmail.com>
2025-11-28 19:51:58 -0300

    arrumado problemas de merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import model.entities.Veiculo;
 
 public class Main {
     public static void main(String[] args) {
-        Veiculo veiculoTeste = new Veiculo("SEM3T12","HONDA","CIVIC", true,"2016");
 
         VeiculoDAO veiculoDAO = new VeiculoDAO();
 //        veiculoDAO.SalvarVeiculo(veiculoTeste);
