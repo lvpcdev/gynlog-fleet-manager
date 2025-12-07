43d9b6f35dc7f951facbc35f9d4afcc38d387b4f
Lucas Vicente <lucasvpc47@gmail.com>
2025-12-07 16:26:17 -0300

    finalizando

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
-import persistence.dao.VeiculoDAO;
-import model.entities.Veiculo;
+
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import view.gui.MenuView;
+
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        SwingUtilities.invokeLater(() -> {
+            MenuView tela = new MenuView();
+            tela.setVisible(true);
+        });
     }
 }
--- t
+++
@@ -1,6 +0,0 @@
-Merge branch 'feat/gestao-veiculos' of https://github.com/lvpcdev/gynlog-fleet-manager into feat/relatorios
-# Please enter a commit message to explain why this merge is necessary,
-# especially if it merges an updated upstream into a topic branch.
-#
-# Lines starting with '#' will be ignored, and an empty message aborts
-# the commit.
