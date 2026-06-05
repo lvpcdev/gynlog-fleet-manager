c2cb0da7d3e7df2932c424ec820f5bc3a6a007b0
Lucas Vicente <152820440+lvpcdev@users.noreply.github.com>
2026-06-05 20:27:20 -0300

    Revert " Feat: Adicionando os relatórios 7 e 9"

--- src/main/java/controller/MovimentacaoController.java
+++ src/main/java/controller/MovimentacaoController.java
@@ -73,11 +73,5 @@ public class MovimentacaoController {
         return movimentacaoService.totalMultasPorVeiculo(id, ano);
     }
 
-    public BigDecimal mediaIpvaPorAno(Year ano){
-        return movimentacaoService.mediaIpvaPorAno(ano);
-    }
 
-    public String mediaDespesasPorCategoria() {
-        return movimentacaoService.mediaDespesasPorCategoria();
-    }
 }
